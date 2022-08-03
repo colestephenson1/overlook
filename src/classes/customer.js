615eed4cb8b3c7e1344eb57c8672bf491d316dbb
Cole Stephenson <colestephenson94@gmail.com>
2022-08-03 11:35:48 -0600

    Create class methods  to return all, past, and future bookings

--- src/classes/customer.js
+++ src/classes/customer.js
@@ -1,8 +1,36 @@
+import bookings from '../mockData/mock-bookings';
 class Customer {
   constructor(customer) {
     this.id = customer.id;
     this.name = customer.name;
   }
+
+  returnAllBookings() {
+    return bookings.filter(booking => this.id === booking.userID)
+  }
+
+  returnPastBookings() {
+    return bookings.filter(booking => {
+      if (this.id === booking.userID) {
+        let splitDate = booking.date.split('/').map(date => parseInt(date));
+        if (splitDate[0] <= 2022 && splitDate[1] <= 8  && splitDate[1] <= 3) {
+          return true;
+        }
+      }
+    })
+  }
+
+  returnFutureBookings() {
+    return bookings.filter(booking => {
+      if (this.id === booking.userID) {
+        let splitDate = booking.date.split('/').map(date => parseInt(date));
+        if (splitDate[0] === 2022 && splitDate[1] >= 8  && splitDate[1] >= 3 || splitDate[0] >= 2023 ) {
+          return true;
+        }
+      }
+    })
+  }
+
 }
 
 export default Customer;
--- test/room-test.js
+++ test/room-test.js
@@ -1,4 +1,5 @@
-import { expect } from 'chai';
+import chai from 'chai';
+const expect = chai.expect;
 import Room from '../src/classes/room';
 
 describe('Room', () => {
